54ddf15c8db4bde40d8c4d0e94b59f542af977b8
DmitriyDog <kukshinofff@mail.ru>
2021-04-14 18:22:29 +0500

    По мелочи

---
+++ data/block.py
@@ -0,0 +1,15 @@
+import datetime
+import sqlalchemy
+
+from .db_session import SqlAlchemyBase
+
+
+class Entertain(SqlAlchemyBase):
+    __tablename__ = 'entertain'
+
+    id = sqlalchemy.Column(sqlalchemy.Integer,
+                           primary_key=True, autoincrement=True)
+    title = sqlalchemy.Column(sqlalchemy.String, nullable=False)
+    content = sqlalchemy.Column(sqlalchemy.String, nullable=True)
+    created_date = sqlalchemy.Column(sqlalchemy.DateTime,
+                                     default=datetime.datetime.now)
--- main.py
+++ main.py
@@ -1,11 +1,21 @@
 from flask import Flask, render_template, redirect, jsonify
 from loginform import LoginForm, RegisterForm
+from data import db_session
+from data.users import User
 
 app = Flask(__name__)
 app.config['SECRET_KEY'] = 'Very_Very_secret_key'
 
 
 def main():
+    db_session.global_init("db/libs.db")
+    user = User()
+    user.name = "Пользователь 1"
+    user.about = "биография пользователя 1"
+    user.email = "email@email.ru"
+    db_sess = db_session.create_session()
+    db_sess.add(user)
+    db_sess.commit()
     app.run()
 
 
