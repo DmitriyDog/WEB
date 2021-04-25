55fa2277f047e844abb602b75ba61b04dcda1300
DmitriyDog <kukshinofff@mail.ru>
2021-04-25 20:35:46 +0500

    изменения для heroku (забыл про них)

--- main.py
+++ main.py
@@ -223,4 +223,4 @@ def redact():
 if __name__ == '__main__':
     main()
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='127.0.0.1', port=port)
+    app.run(host='0.0.0.0', port=port)
