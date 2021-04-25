b5a3e0fef362084e5c6397b9cbdc9a8a1fb48dc1
DmitriyDog <kukshinofff@mail.ru>
2021-04-25 11:57:32 +0500

    Немного поправил 2

--- main.py
+++ main.py
@@ -208,4 +208,4 @@ def find_page(tp, name):
 if __name__ == '__main__':
     main()
     port = int(os.environ.get("PORT", 5000))
-    app.run(host='127.0.0.1', port=port)
+    app.run(host='0.0.0.0', port=port)
